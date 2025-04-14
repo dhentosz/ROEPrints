8919486f57bb07c1cc0bf0a618b28862fe66be9d
dh <16125963+dhentosz@users.noreply.github.com>
2025-04-14 14:25:11 -0400

    Imported logo file to resolve issues in static build files.

--- frontend/src/elements/Header.tsx
+++ frontend/src/elements/Header.tsx
@@ -1,11 +1,12 @@
 // Main Header element
 
 import styles from "./Header.module.css";
+import logo from "../images/roeIcon.png";
 
 export default function Header() {
   return (
     <div className={styles.header}>
-      <img src="src/images/roeIcon.png" alt="roeLogo" id={styles.logo}></img>
+      <img src={logo} alt="roeLogo" id={styles.logo}></img>
       <div className={styles.logo_spacer}></div>
       <h1>ActivePrints</h1>
     </div>
