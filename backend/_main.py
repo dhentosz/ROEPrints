f5eeaa3c6c167359d6373d4a4d1ccc54e7dc968a
dh <16125963+dhentosz@users.noreply.github.com>
2025-04-22 17:21:01 -0400

    Wrote entry point for flask server.

--- backend/_main.py
+++ backend/_main.py
@@ -1,1 +1,10 @@
 # Backend server entry point
+
+import os
+from dotenv import load_dotenv
+from server.server import app
+
+load_dotenv()
+
+if __name__ == "__main__":
+    app.run(port=os.getenv("PORT"))
