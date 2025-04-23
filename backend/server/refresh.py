e534a812a4eaf245fb594a23a946875b2e7caca3
dh <16125963+dhentosz@users.noreply.github.com>
2025-04-23 13:14:33 -0400

    Wrote function to refresh JWT for CarbonAPI.

--- backend/server/refresh.py
+++ backend/server/refresh.py
@@ -1,1 +1,8 @@
 # Handles refreshing JWT for CarbonAPI
+
+from tok.authtoken_create import create_api_token
+from tok.sec import secPath
+
+def refreshToken(min):
+    print("refreshTriggered")
+    print(create_api_token(secPath, min))
