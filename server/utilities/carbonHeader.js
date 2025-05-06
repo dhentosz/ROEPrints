1458668f2c6ad7221e89ea3ac99de632c49152b0
dh <16125963+dhentosz@users.noreply.github.com>
2025-05-06 17:35:33 -0400

    Implemented retries for fetch calls.

--- server/utilities/carbonHeader.js
+++ server/utilities/carbonHeader.js
@@ -1,18 +1,36 @@
 // Fetches new jwt for CarbonAPI and refreshes auth header
 
 export async function getCarbonHeader() {
-  try {
-    const res = await fetch(process.env.AUTH_URL);
-    if (!res.ok) {
-      throw new Error(`TokenRefresh-NetworkError: ${res.status}`);
+  const retries = 3;
+  let attempt = 0;
+  let delay = 2000;
+
+  while (attempt < retries) {
+    try {
+      const res = await fetch(process.env.AUTH_URL);
+      if (!res.ok) {
+        throw new Error(`TokenRefresh-NetworkError: ${res.status}`);
+      }
+      const tok = await res.text();
+      console.log("tokenRefreshed");
+
+      const header = { headers: { Authorization: `Bearer ${tok}` } };
+
+      return header;
+    } catch (e) {
+      attempt++;
+      if (attempt === retries)
+        throw new Error("TokenRefresh-MaxRetriesReached");
+
+      console.error(
+        `Attempt ${attempt} failed. Retrying in ${delay / 1000} seconds.`
+      );
+
+      await new Promise((resolve) => setTimeout(resolve, delay));
+
+      delay *= 2;
+
+      console.log(e);
     }
-    const tok = await res.text();
-    console.log("tokenRefreshed");
-
-    const header = { headers: { Authorization: `Bearer ${tok}` } };
-
-    return header;
-  } catch (e) {
-    console.log(e);
   }
 }
--- server/utilities/carbonPrints.js
+++ server/utilities/carbonPrints.js
@@ -1,16 +1,32 @@
 // fetches carbon print queue data. takes in the url and http header as arguments.
 
 export async function getCarbonPrints(url, header) {
-  try {
-    const apiRes = await fetch(url, header);
+  const retries = 3;
+  let attempt = 0;
+  let delay = 2000;
 
-    if (!apiRes.ok) {
-      throw new Error(`CarbonAPI-NetworkError: ${apiRes.status}`);
+  while (attempt < retries) {
+    try {
+      const apiRes = await fetch(url, header);
+
+      if (!apiRes.ok) {
+        throw new Error(`CarbonAPI-NetworkError: ${apiRes.status}`);
+      }
+
+      const carbonJson = await apiRes.json();
+      return carbonJson;
+    } catch (e) {
+      attempt++;
+      if (attempt === retries) throw new Error("CarbonAPI-MaxRetriesReached.");
+
+      console.error(
+        `Attempt ${attempt} failed. Retrying in ${delay / 1000} seconds.`
+      );
+      await new Promise((resolve) => setTimeout(resolve, delay));
+
+      delay *= 2;
+
+      console.log(e);
     }
-
-    const carbonJson = await apiRes.json();
-    return carbonJson;
-  } catch (e) {
-    console.log(e);
   }
 }
